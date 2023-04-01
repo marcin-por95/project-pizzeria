9ffabf6270828adf961660f789d950759353f5af
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-04-01 13:26:14 +0200

    summary basket

--- src/js/script.js
+++ src/js/script.js
@@ -239,14 +239,14 @@
 
       const productSummury = {};
       productSummury.id = thisProduct.data.id;
-      console.log(thisProduct);
+      //console.log(thisProduct);
       productSummury.name = thisProduct.data.name;
       productSummury.amount = parseInt(thisProduct.amountWidget.value);
       productSummury.priceSingle = parseInt(thisProduct.priceSingle);
       productSummury.price = productSummury.amount * productSummury.priceSingle;
       productSummury.params = thisProduct.prepareCartProductParams();
 
-      console.log('productSummary', productSummury);
+    //  console.log('productSummary', productSummury);
 
       return productSummury;
     }
@@ -285,7 +285,7 @@
           }
         }
 
-        console.log(params);
+       // console.log(params);
 
       }
       return params;
@@ -363,25 +363,34 @@
 
       thisCart.products = [];
       thisCart.getElements(element);
-      console.log('New Cart', thisCart);
+     // console.log('New Cart', thisCart);
       thisCart.initActions();
     }
-    getElements(element) {
+    getElements(element){
       const thisCart = this;
-
       thisCart.dom = {};
       thisCart.dom.wrapper = element;
-      console.log(element);
+      console.log('element karty',element);
       thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
       thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
-      console.log(thisCart.dom.productList);
+      thisCart.dom.deliveryFee = thisCart.dom.wrapper.querySelector(select.cart.deliveryFee);
+      thisCart.dom.subtotalPrice = thisCart.dom.wrapper.querySelector(select.cart.subtotalPrice);
+      thisCart.dom.totalPrice = thisCart.dom.wrapper.querySelectorAll(select.cart.totalPrice);
+      thisCart.dom.totalNumber = thisCart.dom.wrapper.querySelector(select.cart.totalNumber);
+      console.log(thisCart.dom.deliveryFee);
     }
     initActions() {
       const thisCart = this;
 
-      thisCart.dom.toggleTrigger.addEventListener('click', function (event) {
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
         event.preventDefault();
         thisCart.dom.wrapper.classList.toggle('active');
+      });
+      thisCart.dom.productList.addEventListener('update', function(){
+        thisCart.update();
+      });
+      thisCart.dom.productList.addEventListener('remove', function(event){
+        thisCart.remove(event.detail.cartProduct);
       });
     }
     add(menuProduct) {
@@ -389,7 +398,7 @@
 
       /* generate HTML based on template */
       const generatedHTML = templates.cartProduct(menuProduct);
-      console.log(generatedHTML);
+     // console.log(generatedHTML);
 
       /* create DOM element using utils.createElementFromHTML */
       const generatedDOM = utils.createDOMFromHTML(generatedHTML);
@@ -397,15 +406,62 @@
 
       /* find menu container */
       const cartContainer = thisCart.dom.productList;
-      console.log(cartContainer);
+     // console.log(cartContainer);
 
       /* add DOM element to menu container */
       cartContainer.appendChild(generatedDOM);
 
       thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
       console.log('thisCart.products', thisCart.products);
+      //console.log('thisCart.products', thisCart.products);
+      thisCart.update();
     }
 
+    update(){
+      const thisCart = this;
+      //setting variable
+      thisCart.deliveryFee = settings.cart.defaultDeliveryFee;
+       let totalNumber = 0;
+       let subtotalPrice = 0;
+
+       //delete delivery fee if cart is empty
+       if (thisCart.products.length === 0){
+         thisCart.deliveryFee = 0;
+       }
+
+       // change variables for every product in basket
+       for (let product of thisCart.products){
+         const amount = product.amount;
+
+        const price = product.price;
+        totalNumber += amount;
+        subtotalPrice += price;
+      }
+      //push values of variables to properities of cart & dom.innerHTML
+      thisCart.totalPrice = subtotalPrice + thisCart.deliveryFee;
+      thisCart.dom.deliveryFee.innerHTML = thisCart.deliveryFee;
+      thisCart.dom.subtotalPrice.innerHTML = subtotalPrice;
+      for (let totalprice of thisCart.dom.totalPrice){
+        totalprice.innerHTML = thisCart.totalPrice;
+      }
+      thisCart.dom.totalNumber.innerHTML = totalNumber;
+      console.log(thisCart);
+    }
+    remove(product){
+      const thisCart = this;
+      //remove product from HTML
+      product.dom.wrapper.remove();
+       //delete informations from thisCart.products
+       const indexOfProduct = thisCart.products.indexOf(product);
+       thisCart.products.splice(indexOfProduct,1);
+       console.log('after removed',thisCart);
+       thisCart.update();
+
+       //update basket
+
+       thisCart.update();
+       console.log('after updating', thisCart);
+    }
   }
 
   class CartProduct {
@@ -420,11 +476,11 @@
       thisCartProduct.price = menuProduct.price;
       thisCartProduct.priceSingle = menuProduct.priceSingle;
 
-      console.log('thisCardProduct', thisCartProduct);
+     // console.log('thisCardProduct', thisCartProduct);
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget(element);
-      thisCartProduct.initActions();
+      
     }
     getElements(element) {
       const thisCartProduct = this;
@@ -437,7 +493,7 @@
       thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
       thisCartProduct.dom.amountWidgetElem = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
 
-      console.log('CartProduct', thisCartProduct.dom);
+      //console.log('CartProduct', thisCartProduct.dom);
     }
     initAmountWidget() {
       const thisCartProduct = this;
@@ -459,7 +515,7 @@
   const app = {
     initMenu: function () {
       const thisApp = this;
-      console.log('thisApp.data', thisApp.data);
+     // console.log('thisApp.data', thisApp.data);
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
