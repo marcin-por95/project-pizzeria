0d7a9dfc3d5c5c4f0c1df9a0226b231279167557
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-04-02 20:47:17 +0200

    final version od functionality basket

--- src/js/script.js
+++ src/js/script.js
@@ -77,6 +77,11 @@
       defaultDeliveryFee: 20,
     },
     // CODE ADDED END
+    db: {
+      url: '//localhost:3131',
+      products: 'products',
+      orders: 'orders',
+    },
   };
 
   const templates = {
@@ -548,12 +553,25 @@
       const thisApp = this;
      // console.log('thisApp.data', thisApp.data);
       for (let productData in thisApp.data.products) {
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
     initData: function () {
       const thisApp = this;
-      thisApp.data = dataSource;
+      thisApp.data = {};
+      
+      const url = settings.db.url + '/' + settings.db.products;
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse', parsedResponse);
+          thisApp.data.products = parsedResponse;
+          thisApp.initMenu();
+        });
+      console.log('dane',thisApp.data, JSON.stringify(thisApp.data));
+
     },
     initCart: function () {
       const thisApp = this;
@@ -569,7 +587,7 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
       thisApp.initData();
-      thisApp.initMenu();
+      
       thisApp.initCart();
     },
   };
