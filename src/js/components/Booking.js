695fb05d3e0ba6fbd579200a611db225d766b2cc
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-04-15 10:39:12 +0200

    creating 2 new plugin

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import {select, templates} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import HourPicker from './HourPicker.js';
+import DatePicker from './DatePicker.js';
 
 export default class Booking {
   constructor(element) {
@@ -8,16 +10,28 @@ export default class Booking {
   }
 
   initWidgets() {
+
+    this.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
+    this.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
     this.peopleAmountWidget = new AmountWidget(this.dom.peopleAmount);
     this.hoursAmountWidget = new AmountWidget(this.dom.hoursAmount);
+
+    this.dom.hourPicker = document.querySelector(select.widgets.hourPicker.wrapper);
+    this.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
     this.dom.hoursAmount.addEventListener('update', () => {});
     this.dom.peopleAmount.addEventListener('update', () => {});
+    this.hourPicker = new HourPicker(this.dom.hourPicker);
+    this.datePicker = new DatePicker(this.dom.datePicker);
   }
 
   render(element) {
     this.dom = {};
     this.dom.wrapper = element;
     this.dom.wrapper.innerHTML = templates.bookingWidget();
+    this.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
+    this.dom.hoursAmount  = document.querySelector(select.booking.hoursAmount);
+    this.dom.hourPicker   = document.querySelector(select.widgets.hourPicker.wrapper);
+    this.dom.datePicker   = document.querySelector(select.widgets.datePicker.wrapper);
 
     this.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
     this.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
