3af78364af43c53f12154f385d91349e5ab6e1a3
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-04-18 01:27:59 +0200

    create parseData and makeBooked

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,4 @@
-import {select, settings, templates} from '../settings.js';
+import {select, settings, templates, classNames} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
 import HourPicker from './HourPicker.js';
 import DatePicker from './DatePicker.js';
@@ -9,11 +9,14 @@ export default class Booking {
     this.render(element);
     this.initWidgets();
     this.getData();
+
+    this.parseData = this.parseData.bind(this);
+    this.getData = this.getData.bind(this);
   }
 
   getData() {
-    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(this.datePicker.minDate);
-    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(this.datePicker.maxDate);
+    const startDateParam = `${settings.db.dateStartParamKey}=${utils.dateToStr(this.datePicker.minDate)}`;
+    const endDateParam = `${settings.db.dateEndParamKey}=${utils.dateToStr(this.datePicker.maxDate)}`;
 
 
     const params = {
@@ -56,30 +59,65 @@ export default class Booking {
         const eventsCurrentResponse = allResponses[1];
         const eventsRepeatResponse = allResponses[2];
 
-        return  Promise.all([
+        return Promise.all([
           bookingsResponse.json(),
           eventsCurrentResponse.json(),
           eventsRepeatResponse.json(),
         ]);
       })
-      .then(function ([bookings, eventsCurrent, eventsRepeat ]) {
-        console.log(bookings);
-        console.log(eventsCurrent);
-        console.log(eventsRepeat);
-      });
+
+      .then(function ([bookings, eventsCurrent, eventsRepeat]) {
+        this.parseData(bookings, eventsCurrent, eventsRepeat);
+      }.bind(this));
+
+
   }
 
-  initWidgets() {
+  parseData(bookings, eventsCurrent, eventsRepeat) {
 
-    this.peopleAmountWidget = new AmountWidget(this.dom.peopleAmount);
-    this.hoursAmountWidget = new AmountWidget(this.dom.hoursAmount);
-    this.dom.hoursAmount.addEventListener('update', () => {
-    });
-    this.dom.peopleAmount.addEventListener('update', () => {
-    });
-    this.hourPicker = new HourPicker(this.dom.hourPicker);
-    this.datePicker = new DatePicker(this.dom.datePicker);
+    this.booked = {};
+
+    for (let item of bookings) {
+      this.makeBooked(item.date, item.hour, item.duration, item.table);
+    }
+
+    for (let item of eventsCurrent) {
+      this.makeBooked(item.date, item.hour, item.duration, item.table);
+    }
+
+    const minDate = this.datePicker.minDate;
+    const maxDate = this.datePicker.maxDate;
+
+    for (let item of eventsRepeat) {
+      if (item.repeat === 'daily') {
+        for (let loopDate = minDate; loopDate <= maxDate; loopDate = utils.addDays(loopDate, 1)) {
+          this.makeBooked(utils.dateToStr(loopDate), item.hour, item.duration, item.table);
+        }
+      }
+    }
+    console.log('this.booked', this.booked);
   }
+
+  makeBooked(date, hour, duration, table) {
+
+
+    if (typeof this.booked[date] === 'undefined') {
+      this.booked[date] = {};
+    }
+
+    const startHour = utils.hourToNumber(hour);
+
+    for(let hourBlock = startHour; hourBlock < startHour + duration; hourBlock += 0.5){
+     // console.log('loop', hourBlock);
+      if (typeof this.booked[date][hourBlock] === 'undefined') {
+        this.booked[date][hourBlock] = [];
+      }
+
+      this.booked[date][hourBlock].push(table);
+    }
+  }
+
+
 
   render(element) {
     this.dom = {};
@@ -91,4 +129,19 @@ export default class Booking {
     this.dom.hourPicker = document.querySelector(select.widgets.hourPicker.wrapper);
     this.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
   }
+
+  initWidgets() {
+    this.hourPicker = new HourPicker(this.dom.hourPicker);
+    this.datePicker = new DatePicker(this.dom.datePicker);
+    this.peopleAmountWidget = new AmountWidget(this.dom.peopleAmount);
+    this.hoursAmountWidget = new AmountWidget(this.dom.hoursAmount);
+    this.dom.hoursAmount.addEventListener('update', () => {
+    });
+    this.dom.peopleAmount.addEventListener('update', () => {
+    });
+
+
+
+  }
+
 }
